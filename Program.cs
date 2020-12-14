f1a9db9c6d363a17cd9098b35cb8f3d7aa74f013
Your Name <you@example.com>
2020-12-13 22:30:19 -0300

    area do triangulo

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double b, a, af;
+
+            Console.WriteLine("Vamos calcular a area de um triangulo");
+            Console.Write("Digite a altura : ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a base: ");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            af = (a * b)/2;
+            Console.WriteLine($"O Valor da area é de {af}");
+            Console.WriteLine();
+        }
+    }
+}
